e8cbe5aa3baf3ba697758706e4f2f31248b84796
JUAN <juanbr26@gmail.com>
2023-05-23 16:58:23 -0300

    .

--- ARQUIVOS DE PYTHON curso em video/AULA-21.py
+++ ARQUIVOS DE PYTHON curso em video/AULA-21.py
@@ -1,2 +1,18 @@
-import pygame 
+'''import pygame
 
+pygame.init()
+pygame.mixer.music.load('AULA-21.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()'''
+
+'''aula com erro que nao consegui '''
+
+
+import pygame
+print (pygame.ver) 
+pygame.init()
+pygame.mixer.music.load('AULA-21.mp3')
+pygame.mixer.music.play()
+
+pygame.event.wait()
---
+++ ARQUIVOS DE PYTHON curso em video/AULA-22.py
@@ -0,0 +1,15 @@
+nome = str(input('QUAL SEU NOME: ')).strip()
+## strip para contar os espaços
+print ('ANALISANDO SEU NOME {}'.format(nome))
+print('SEU NOME EM MAIUSCULAS E {}'.format(nome.upper()))
+'''upper para saber ser a letra e maiuscula '''
+print('SEU NOME EM MINUSCULAS E {}'.format(nome.lower()))
+'''lower para deixa seu nome em minusculas'''
+print('SEU NOME TEM AO TODO {} LETRAS '.format(len(nome) - nome.count(' ')))
+'''PARA CONTAR SEU NOME INTEIRO'''
+
+#print ('SEU PRIMEIRO NOME TEM {} LETRAS'.format(nome.find(' ')))
+
+'''PARA ENCONTRAR QUAL É A POSIÇÃO DO PRIMEIRO ESPAÇO'''
+separa = nome.split()
+print('SEU PRIMEIRO NOME E {} E ELE TEM {} LETRAS'.format(separa[0], len(separa[0])))
