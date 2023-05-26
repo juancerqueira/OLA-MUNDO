e97aa7e3d74c8adbaa4def2f6871ce9e98220cc6
JUAN <juanbr26@gmail.com>
2023-05-26 00:08:27 -0300

    -

--- ARQUIVOS DE PYTHON curso em video/AULA-28.py
+++ ARQUIVOS DE PYTHON curso em video/AULA-28.py
@@ -1,9 +1,25 @@
-import random
+'''import random
 numero = [0,1,2,3,4,5]
 escolhido = random.choice(numero)
 
 seunumero = int(input('Digite um numero de 0 a 5: '))
 if seunumero == escolhido:
     print('Parabéns, você acertou')
+    print ('--FIM--')  
 else:
-     print(f'voce errou, o numero escolhido foi {escolhido}.')    
+     print(f'veu ganhei o numero que pensei foi {escolhido}.')   '''
+
+
+from random import randint
+computador = randint(0, 5)  
+ #FAZ O COMPUTADOR SORTEAR UM NUMERO
+jogador = int(input('Escolar um numero de 0 a 5: '))
+if jogador == computador:
+    print('-=-'*20)
+    print('Parabéns, você acertou') 
+    print('-=-'*20)
+    print ('--FIM--')
+else:
+    print('-=-'*20)
+    print('GAME OVER')
+    print('-=-'*20) 
