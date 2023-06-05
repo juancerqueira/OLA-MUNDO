11a4ea6d312b70ebb31f1b0fb3ad1db5a955f51a
JUAN <135564496+jc26br@users.noreply.github.com>
2023-06-05 00:45:14 -0300

    Create AULA-TEORICA-13.py

---
+++ AULA TEORICA EXERCICIOS/AULA-TEORICA-13.py
@@ -0,0 +1,20 @@
+'''ESTRUTURA DE REPETIÇÃO'''
+'''FOR C IN RANGE ()'''
+'''for c in range(6, 2, -2):
+    print (c)
+print('fim')'''
+
+
+'''
+inicio = int(input('inicio: '))
+fim = int(input('fim: '))
+passo = int(input('passo: '))
+for ccom in range(inicio,fim+1,passo):
+    print(ccom)
+print('fim')
+'''
+s = 0
+for c in range(0,4):
+     n = int(input('digite: '))
+     s += n
+print(f'a soma {s}')
